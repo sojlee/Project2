71f9689a8f8fa117f21d4f487ec6a8c92021daef
sojlee <720sojung@gmail.com>
2020-05-19 22:07:06 -0700

    remove test string

--- main.c
+++ main.c
@@ -34,9 +34,7 @@ void filestat1(){
 
 //파일 2의 정보를 가져오는 함수 작성
 void filestat2(){
-    stat("text2", &stat2);
-    printf("size : %d \n", (int)stat2.st_size);
-    printf("size : %d \n", (int)stat2.st_blocks);	
+    stat("text2", &stat2);	
 }
 
 //파일 1의 시간 정보를 가져오는 함수 작성
